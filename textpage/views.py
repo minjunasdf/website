b35fea875a07499752225fbbdf67dd9cfeabfbcf
김민준 <minjungim383@gmail.com>
2023-03-03 12:12:36 +0900

    사이트 환경 수정

--- textpage/views.py
+++ textpage/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import Text, Comment
 from django.utils import timezone
 from .forms import TextForm
+from django.contrib.auth.decorators import login_required
 
 
 def index(request):
@@ -15,12 +16,14 @@ def detail(request, text_id):
     content = {'text' : text}
     return render(request, 'textpage/text_detail.html', content)
 
+@login_required(login_url='common:login')
 def comment_create(request, text_id):
     text = get_object_or_404(Text, pk=text_id)
     comment = Comment(texts = text, content=request.POST.get('content'), create_date=timezone.now())
     comment.save()
     return redirect('textpage:detail', text_id=text.id)
 
+@login_required(login_url='common:login')
 def text_create(request):
     if request.method == 'POST':
         form = TextForm(request.POST)
