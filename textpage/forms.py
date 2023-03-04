c676167623f5d56876bfbc6c7a972c9ffe9607fc
home <minjungim383@gmail.com>
2023-03-04 23:33:38 +0900

    사이트 환경 수정

--- textpage/forms.py
+++ textpage/forms.py
@@ -1,8 +1,20 @@
 from django import forms
-from textpage.models import Text
+from textpage.models import Text, Comment
 
 
 class TextForm(forms.ModelForm):
     class Meta:
         model = Text
         fields = ['subject', 'content']
+        labels = {
+            'subject' : '글 제목',
+            'content': '글 내용',
+        }
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['content']
+        labels = {
+            'content': '답변내용',
+        }
--- textpage/views.py
+++ textpage/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Text, Comment
 from django.utils import timezone
-from .forms import TextForm
+from .forms import TextForm, CommentForm
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseNotAllowed
 
 
 def index(request):
@@ -19,9 +20,19 @@ def detail(request, text_id):
 @login_required(login_url='common:login')
 def comment_create(request, text_id):
     text = get_object_or_404(Text, pk=text_id)
-    comment = Comment(texts = text, content=request.POST.get('content'), create_date=timezone.now(), author=request.user)
-    comment.save()
-    return redirect('textpage:detail', text_id=text.id)
+        if request.method == "POST":
+            form = CommentForm(request.POST)
+            if form.is_valid():
+                answer = form.save(commit=False)
+                answer.create_date = timezone.now()
+                answer.content = text
+                answer.author = request.user
+                answer.save()
+                return redirect('textpage:detail', question_id=text.id)
+        else:
+            return HttpResponseNotAllowed('Only POST is possible.')
+        context = {'texts': text, 'form': form}
+        return render(request, 'textpage/text_detail.html', context)
 
 @login_required(login_url='common:login')
 def text_create(request):
