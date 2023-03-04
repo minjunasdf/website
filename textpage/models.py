125c273ca851cacd89182a18478bba81cdb8b5ab
home <minjungim383@gmail.com>
2023-03-04 23:12:36 +0900

    사이트 환경 수정

--- textpage/models.py
+++ textpage/models.py
@@ -6,7 +6,7 @@ class Text(models.Model):
     subject = models.CharField(max_length=200)
     content = models.TextField()
     create_date = models.DateTimeField()
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.subject
@@ -16,4 +16,4 @@ class Comment(models.Model):
     texts = models.ForeignKey(Text, on_delete=models.CASCADE)
     content = models.TextField()
     create_date = models.DateTimeField()
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
