90a6fb90af867430165fd7bb2a5764e245f5a37f
home <minjungim383@gmail.com>
2023-03-05 20:36:53 +0900

    사이트 환경 수정

--- mainpage/views.py
+++ mainpage/views.py
@@ -3,3 +3,7 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'mainpage/main.html')
+
+
+def permissionerror(request):
+    return render(request, '403.html')
