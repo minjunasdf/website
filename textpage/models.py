25e6f7e3191aca409f79e51110bf987739f04acb
김민준 <minjungim383@gmail.com>
2023-03-06 16:17:25 +0900

    사이트 환경 수정

--- textpage/models.py
+++ textpage/models.py
@@ -7,8 +7,8 @@ class Text(models.Model):
     content = models.TextField()
     create_date = models.DateTimeField()
     author = models.ForeignKey(User, on_delete=models.CASCADE, null=True, related_name='author_text')
-    goodvote = models.ManyToManyField(User, related_name='text_goodvote', null=True)
-    badvote = models.ManyToManyField(User, related_name='text_badvote', null=True)
+    goodvote = models.ManyToManyField(User, related_name='text_goodvote')
+    badvote = models.ManyToManyField(User, related_name='text_badvote')
 
     def __str__(self):
         return self.subject
