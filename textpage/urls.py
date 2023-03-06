38908a20902ec8f644f97d5f41ccc5a5fad06d68
김민준 <minjungim383@gmail.com>
2023-03-06 11:51:31 +0900

    사이트 환경 수정

--- textpage/urls.py
+++ textpage/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('<int:text_id>/', views.detail, name='detail'),
     path('comment/create/<int:text_id>/', views.comment_create, name='comment_create'),
     path('text/goodvote/<int:text_id>/', views.text_goodvote, name='text_goodvote'),
-path('text/badvote/<int:text_id>/', views.text_badvote, name='text_badvote'),
+    path('text/badvote/<int:text_id>/', views.text_badvote, name='text_badvote'),
     path('text/create/', views.text_create, name='text_create'),
 ]
